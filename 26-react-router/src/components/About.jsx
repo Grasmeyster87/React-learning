eb9720bf86191683881dd4ac6114af24bbbd66d9
Grasmeyster87 <grasmeyster@gmail.com>
2024-07-26 14:35:20 +0300

    26.07.2024 - Navigation React Router Finish

--- 26-react-router/src/components/About.jsx
+++ 26-react-router/src/components/About.jsx
@@ -1,8 +1,17 @@
 
-const About = () => {
-    return (
-        <h1>About</h1>
-    );
-};
 
+const About = () => {
+    return <>
+        <h1>About</h1>
+    </>;
+};
+/*
+import { Link } from "react-router-dom";
+
+Link - компонент для внутрених ссылок. 
+Даёт возможность не загружать HTML страницы из сервера
+
+        <Link to="/">Go to home</Link><br />
+        <Link to="/contacts">Go to Contacts</Link>
+*/
 export default About;
---
+++ 26-react-router/src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+
+const Menu = () => {
+    return (
+        <nav>
+            <NavLink style={({ isActive }) => isActive ? { color: 'lightyellow', textDecoration: 'none' } : {}} to="." end>Home</NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'activeLink' : 'Link'} to="about">About</NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'activeLink' : 'Link'} to="contacts">Contacts</NavLink>
+        </nav>
+    );
+};
+/*
+свойство end добавляется для того чтобы снять выделение(сделать не активным)
+ с index пункта меню home
+*/
+export default Menu;
