b85480e02119c837751235f47dd59e8a7547fbb8
Grasmeyster87 <grasmeyster@gmail.com>
2024-08-17 20:11:43 +0300

    17.08.2024 - redux store.subscribe

---
+++ 29-pure-redux/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from 'https://cdn.jsdelivr.net/npm/@reduxjs/toolkit@1.9.5/+esm';
+
+import reducer from './reducer.js';
+
+export default configureStore({
+    reducer: reducer
+});
+
