82a307f3cb381b43dd611a83f0932ae097cc1b79
Grasmeyster87 <grasmeyster@gmail.com>
2024-08-18 20:24:23 +0300

    18.08.2024 - clear all times

--- 29-pure-redux/index.js
+++ 29-pure-redux/index.js
@@ -1,6 +1,34 @@
 import store from './redux/store.js';
+import getCurrentTime from './utils/getCurrentTime.js';
 
-const unsubscribe = store.subscribe(() => console.log(`Redux store just changed! ${store.getState()}`));
+const addTimeBtn = document.getElementById('addTime');
+
+addTimeBtn.addEventListener('click', () => {
+    store.dispatch({
+        type: 'ADD_CURRENT_TIME',
+        payload: getCurrentTime()
+    });
+});
+
+const clearTimesBtn = document.getElementById('clearTimes');
+clearTimesBtn.addEventListener('click', () => {
+    store.dispatch({
+        type: 'CLEAR_ALL_TIMES'
+    })
+})
+
+const timesList = document.getElementById('timesList');
+
+store.subscribe(() => {
+    timesList.innerHTML = '';
+    const times = store.getState();
+    times.forEach((time) => {
+        let li = document.createElement('li');
+        li.innerText = time;
+        timesList.appendChild(li);
+    });
+})
+/* const unsubscribe = store.subscribe(() => console.log(`Redux store just changed! ${store.getState()}`));
 
 store.dispatch({
     type: 'ADD_CURRENT_TIME',
@@ -17,3 +45,4 @@ store.dispatch({
 store.dispatch({
     type: 'CLEAR_ALL_TIMES',
 });
+ */
