8c013826d83c3bf60e0942fd54fd3fa46fc21885
Grasmeyster87 <grasmeyster@gmail.com>
2024-08-29 10:45:00 +0300

    Added thunkFunction

--- 30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ 30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -1,10 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import axios from 'axios';
-//import { v4 as uuidv4 } from 'uuid';
 import createBookWithID from '../../utils/createBookWithID';
-//import { addBook } from '../../redux/books/actionCreactors';
-import { addBook } from '../../redux/slices/booksSlice';
+import { addBook, thunkFunction } from '../../redux/slices/booksSlice';
 import booksData from '../../data/books.json';
 import './BookForm.css';
 
@@ -29,17 +26,13 @@ const BookForm = () => {
             setAuthor(''); // сбрасуем поля 
         }
     };
-    const handleAddRandomBookViaAPI = async () => {
-        try {
 
-            const res = await axios.get('http://localhost:4000/random-book');
-            if (res?.data?.title && res?.data?.author) {
-                dispatch(addBook(createBookWithID(res.data, 'API')));
-            }
 
-        } catch (error) {
-            console.log('Error fetching random book', error);
-        }
+    
+
+
+    const handleAddRandomBookViaAPI = () => {
+        dispatch(thunkFunction);
     };
 
     return (
